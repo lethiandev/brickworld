e8cca44eafbe5f39660335e2a15d59d2eceabe1d
Konrad Nowakowski <konrad.x92@gmail.com>
2021-02-12 17:45:29 +0000

    Fix minor spellings

--- src/scripts/shared_constants.gd
+++ src/scripts/shared_constants.gd
@@ -2,7 +2,7 @@ extends Node
 class_name SharedConstants
 tool
 
-# Brick world dimmensions
-# Constancts are shared across different systems
+# Brick world dimensions
+# Constants are shared across different systems
 const BRICK_BASE: Vector3 = Vector3(1.0, 0.5, 1.0)
-const BRICK_SCALE: float = 1.0 / 8.0 
+const BRICK_SCALE: float = 1.0 / 8.0
